c54306d7251059a8acf101f5d5ecd7fd6a384e51
Shobhit Kushwaha <shobhitkushwaha1406@gmail.com>
2024-02-10 11:28:09 +0530

    day 21

---
+++ Day 21/125_Valid_palindrome.cpp
@@ -0,0 +1,26 @@
+// TC - O(n)
+// SC - O(1)
+class Solution
+{
+public:
+    bool isPalindrome(string s)
+    {
+        int left = 0;
+        int right = s.length() - 1;
+        while (left < right)
+        {
+            if (!isalnum(s[left]))
+                left++;
+            else if (!isalnum(s[right]))
+                right--;
+            else if (tolower(s[left]) != tolower(s[right]))
+                return false;
+            else
+            {
+                left++;
+                right--;
+            }
+        }
+        return true;
+    }
+};
